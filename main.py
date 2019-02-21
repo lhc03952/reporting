fc9f94e1be92c62e02882eb0eae4a5aeb0355887
lhc03952 <34283364+lhc03952@users.noreply.github.com>
2019-02-21 12:33:46 +0800

    Update main.py
    優化效能

--- main.py
+++ main.py
@@ -8,20 +8,17 @@ import reporting
 import threading
 
 def main():
-    while(1):
-        print(str(datetime.datetime.now()))
-        login_thread = threading.Thread(target = conn,args = ('118.233.34.140', 6901,'登入伺服器'))
-        char_thread = threading.Thread(target = conn, args = ('118.233.34.140', 6122,'角色伺服器'))
-        map_thread = threading.Thread(target = conn, args = ('118.233.34.140', 5122,'地圖伺服器'))
-        login_thread.start()
-        char_thread.start()
-        map_thread.start()
-        login_thread.join()
-        char_thread.join()
-        map_thread.join()
-        time.sleep(5)
+    print(str(datetime.datetime.now()))
+    login_thread = threading.Thread(target = conn,args = ('118.233.34.140', 6901,'登入伺服器'))
+    char_thread = threading.Thread(target = conn, args = ('118.233.34.140', 6122,'角色伺服器'))
+    map_thread = threading.Thread(target = conn, args = ('118.233.34.140', 5122,'地圖伺服器'))
+    login_thread.start()
+    char_thread.start()
+    map_thread.start()
+    login_thread.join()
+    char_thread.join()
+    map_thread.join()
     
-    if(1):
       """  try:
             address = ('118.233.34.140', 6901)
         except:
@@ -37,7 +34,7 @@ def conn(ip, port, typ):
         address = (ip, port)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(address)
-        s.close()
+#        s.close()
         print(typ+' 正常\n')
     except:
         print(typ+' 異常\n')
